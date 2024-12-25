38182baf8d19d3a4b30855f1fdfb93dcd667d993
sagarverma40 <kumkm1864@gmail.com>
2024-12-26 02:20:03 +0530

    Update preprocessor.py

--- preprocessor.py
+++ preprocessor.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-import plotly.express as px
+
 # Function to create filter multiselect options in Streamlit
 def multiselect(title, options_list):
     selected = st.sidebar.multiselect(title, options_list)
